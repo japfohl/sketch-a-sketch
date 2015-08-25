9d3acd91f48cd71212e7efe03570f9f7f3f728b2
Joe Pfohl <joepfohl@gmail.com>
2015-08-25 14:03:48 -0400

    Grid generates and styles correctly

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,21 @@
-var gridSize = 16;
+var numBlocks = +prompt("Please specifiy a grid size between 16-128"), 
+	blockSize = Math.round(500 / numBlocks),
+	gridWidth = (blockSize*numBlocks)+numBlocks, 
+	gridHeight = blockSize*numBlocks;
 
 $(document).ready(function() {
 
-	for (var i = 1; i <= gridSize; i++) {
+	//generate grid of <div>s based on numBlocks
+
+	for (var i = 1; i <= numBlocks; i++) {
 		$('#container').append('<div class="row" id="row'+i+'"></div>');
-			for (var j = 1; j <= gridSize; j++) {
+			for (var j = 1; j <= numBlocks; j++) {
 				$('#row' + i).append('<div class="block" id="block'+i+j+'"></div>');
 			}
 	}
 
+	//style grid elements
+	$('#container').css({'height': gridHeight, 'width': gridWidth});
+	$('.row').css({'height': blockSize, 'width': gridWidth});
+	$('.block').css({'height': blockSize, 'width': blockSize});
 });
