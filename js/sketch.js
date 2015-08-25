5f26b81f727b213927a8c6c4394937db1569b898
Joe Pfohl <joepfohl@gmail.com>
2015-08-25 12:39:50 -0400

    JS now properly generates and adds divs to the DOM based on how gridSize is defined

--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,11 @@ var gridSize = 16;
 
 $(document).ready(function() {
 
-
+	for (var i = 1; i <= gridSize; i++) {
+		$('#container').append('<div class="row" id="row'+i+'"></div>');
+			for (var j = 1; j <= gridSize; j++) {
+				$('#row' + i).append('<div class="block" id="block'+i+j+'"></div>');
+			}
+	}
 
 });
