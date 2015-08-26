86c783f48800071d6922089ceea779a318e0dc51
Joe Pfohl <joepfohl@gmail.com>
2015-08-26 11:32:43 -0400

    tests for values outside proper range. Grid adds background color on mousehover

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,31 @@
-var numBlocks = +prompt("Please specifiy a grid size between 16-128"), 
+var numBlocks = getNumBlocks(), 
 	blockSize = Math.round(500 / numBlocks),
 	gridWidth = (blockSize*numBlocks)+numBlocks, 
 	gridHeight = blockSize*numBlocks;
 
+function getNumBlocks() {
+	var tempSize, badSize = true;
+
+	do {
+		var tempSize = +prompt("Please specifiy a grid size between 16-64", 16);
+
+		if (tempSize >= 16 && tempSize <= 64) {
+			badSize = false;
+		} else {
+			alert("Your choice of " + tempSize + " is outside the specified range. Please try again.");
+		}
+	} while (badSize)
+
+	return tempSize;
+
+}
+
+function addBlackBackground () {
+	$(this).addClass('black-background');
+}
+
 $(document).ready(function() {
-
 	//generate grid of <div>s based on numBlocks
-
 	for (var i = 1; i <= numBlocks; i++) {
 		$('#container').append('<div class="row" id="row'+i+'"></div>');
 			for (var j = 1; j <= numBlocks; j++) {
@@ -18,4 +37,7 @@ $(document).ready(function() {
 	$('#container').css({'height': gridHeight, 'width': gridWidth});
 	$('.row').css({'height': blockSize, 'width': gridWidth});
 	$('.block').css({'height': blockSize, 'width': blockSize});
+
+	//added hover effect of black background when mouse enters div
+	$('.block').hover(addBlackBackground);
 });
