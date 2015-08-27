19a3f1d4fa87446b4c399504674371cd5e4baacd
Joe Pfohl <joepfohl@gmail.com>
2015-08-27 15:26:20 -0400

    add all content

---
+++ README.md
@@ -0,0 +1,9 @@
+# sketch-a-sketch
+
+Project: Javascript/jQuery
+
+Fancy sketch-a-sketch tool using HTML / CSS / JS / jQuery.
+
+http://www.theodinproject.com/web-development-101/javascript-and-jquery
+
+
---
+++ js/sketch.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+	buildGrid();
+	setBackground();
+	$('#reset').click(restart);
+	$('#clear').click(clearGrid);
+	$('#default').click(blackMode);
+	$('#rainbow').click(rainbowMode);
+});
+
+var background = {
+		black: function () {
+			$(this).css('background-color', 'rgb(0,0,0)');
+		}, 
+		random: function () {
+			$(this).css('background-color', 'rgb('+getRGB()+', '+getRGB()+', '+getRGB()+')');
+		}
+	}, 
+	backgroundColor = 'black', 
+	currentOpacity; //starts at default color of black
+
+function getNumBlocks() {
+	var tempSize, badSize = true;
+	//test for proper grid size and deny if outside specified range
+	do {
+		tempSize = +prompt("Please specifiy a grid size between 16-64", 16);
+		if (tempSize >= 16 && tempSize <= 64) {
+			badSize = false;
+		} else {
+			alert("Your choice of " + tempSize + " is outside the specified range. Please try again.");
+		}
+	} while (badSize)
+	return tempSize;
+}
+
+function buildGrid() {
+	//set up grid dimensions
+	var numBlocks = getNumBlocks(), 
+	blockSize = Math.round(500 / numBlocks),
+	gridWidth = (blockSize*numBlocks)+numBlocks, 
+	gridHeight = blockSize*numBlocks;
+	//generate grid
+	for (var i = 1; i <= numBlocks; i++) {
+		$('#container').append('<div class="row" id="row'+i+'"></div>');
+			for (var j = 1; j <= numBlocks; j++) {
+				$('#row' + i).append('<div class="block" id="block'+i+j+'"></div>');
+			}
+	}
+	//style grid borders
+	$('#container').css({'height': gridHeight, 'width': gridWidth});
+	$('.row').css({'height': blockSize, 'width': gridWidth});
+	$('.block').css({'height': blockSize, 'width': blockSize});
+}
+
+function getRGB() {
+	return Math.floor(Math.random()*266);
+}
+
+function setBackground () {
+	$('.block').mouseenter(background[backgroundColor]);
+}
+
+function restart () {
+	$('#container').empty();
+	$('.btn, #fade').removeClass('selected-button');
+	$('#fade').html('Toggle Fade Mode');
+	buildGrid();
+	backgroundColor = 'black';
+	$('#default').addClass('selected-button');
+	setBackground();
+}
+
+function clearGrid () {
+	$('.block').css('background-color', '#fff');
+}
+
+function blackMode () {
+	$('.btn').removeClass('selected-button');
+	$(this).addClass('selected-button');
+	backgroundColor = 'black';
+	setBackground();
+}
+
+function rainbowMode () {
+	$('.btn').removeClass('selected-button');
+	$(this).addClass('selected-button');
+	backgroundColor = 'random';
+	setBackground();
+}
